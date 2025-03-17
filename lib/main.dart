d4cb4b243bd6daa337b5b3ac0c820e4574f0e680
rofidarashed <rofidaelmaghraby@gmail.com>
2025-03-17 13:40:33 +0200

    fixed login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:uber/pages/intro_page.dart';
+import 'package:uber/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Uber',
 
-      home: const IntroPage(),
+      home: const LoginPage(),
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,44 +15,49 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       appBar: AppBar(backgroundColor: white),
       backgroundColor: white,
-      body: Center(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.only(
-            left: MediaQuery.of(context).size.width / 8,
-            right: MediaQuery.of(context).size.width / 8,
-            top: MediaQuery.of(context).size.height / 5,
-            bottom: MediaQuery.of(context).size.height / 15,
-          ),
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height,
-            // width: MediaQuery.of(context).size.width*0.82587,
-            child: Column(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.all(50),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 2,
+                      height: MediaQuery.of(context).size.height * 0.132,
+                      child: Image.asset(
+                        "assets/logoDark.png",
+                        width: double.minPositive,
+                      ),
+                    ),
+                    Text(
+                      'Login to your account.',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(height: 100),
+                    InputTextButton(
+                      labelText: 'E-mail',
+                      hintText: 'qwerty@gmail.com',
+                    ),
+                    InputTextButton(labelText: 'Password', hintText: '*****'),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Forgot your password?',
+                        style: TextStyle(color: black),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Column(
               children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width / 2,
-                  height: MediaQuery.of(context).size.height * 0.132,
-                  child: Image.asset(
-                    "assets/logoDark.png",
-                    width: double.minPositive,
-                  ),
-                ),
-                Text('Login to your account.', style: TextStyle(fontSize: 20)),
-                Spacer(flex: 2),
-                InputTextButton(
-                  labelText: 'E-mail',
-                  hintText: 'qwerty@gmail.com',
-                ),
-                InputTextButton(labelText: 'Password', hintText: '*****'),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Forgot your password?',
-                    style: TextStyle(color: black),
-                  ),
-                ),
+                ElevatedButton(onPressed: () {}, child: Text("test")),
               ],
             ),
-          ),
+          ],
         ),
       ),
     );
