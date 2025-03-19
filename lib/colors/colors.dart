19f087092dc1f90f1ee25cb7c04f75438c78d58a
rofidarashed <rofidaelmaghraby@gmail.com>
2025-03-19 08:09:23 +0200

    add new shades of gray

--- lib/colors/colors.dart
+++ lib/colors/colors.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 Color white = Colors.white;
 Color black = Colors.black;
 Color gray = const Color.fromRGBO(217, 217, 217, 1);
+Color gray0 = const Color.fromRGBO(236, 236, 236, 1);
 Color gray1 = Color.fromRGBO(167, 167, 167, 1);
+Color gray2 = Color.fromRGBO(122, 122, 122, 1);
+
