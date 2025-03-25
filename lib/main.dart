3d42d13b3f53bc82debf9b8da9c0f954f91e80ec
rofidarashed <rofidaelmaghraby@gmail.com>
2025-03-25 17:37:49 +0200

    add globals

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uber/globals.dart';
 import 'package:uber/pages/intro_page.dart';
 
 void main() {
@@ -11,6 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
+      scaffoldMessengerKey: scaffoldMessengerKey,
       debugShowCheckedModeBanner: false,
       title: 'Uber',
 
