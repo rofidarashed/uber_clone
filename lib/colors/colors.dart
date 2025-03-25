7f2ce9749f2164fa16ad6fc354b2658a0230e43f
rofidarashed <rofidaelmaghraby@gmail.com>
2025-03-25 11:35:35 +0200

    add darker shade of gray

--- lib/colors/colors.dart
+++ lib/colors/colors.dart
@@ -6,3 +6,4 @@ Color gray0 = const Color.fromRGBO(236, 236, 236, 1);
 Color gray = const Color.fromRGBO(217, 217, 217, 1);
 Color gray1 = const Color.fromRGBO(167, 167, 167, 1);
 Color gray2 = const Color.fromRGBO(122, 122, 122, 1);
+Color gray3 = const Color.fromRGBO(72, 72, 72, 1);
