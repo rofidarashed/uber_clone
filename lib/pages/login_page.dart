026344daf7156ffed18d5496ffa4dd39531f70c5
rofidarashed <rofidaelmaghraby@gmail.com>
2025-04-27 00:37:53 +0300

    add firebaseAuth to login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,9 @@
 import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:uber/colors/colors.dart';
 import 'package:uber/elements/buttons/black_button.dart';
+import 'package:uber/pages/forgot_password_page.dart';
 import 'package:uber/pages/main_page.dart';
 import 'package:uber/elements/buttons/input_text_button.dart';
 
@@ -16,7 +18,9 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
 
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,6 +66,7 @@ class _LoginPageState extends State<LoginPage> {
                         InputTextButton(
                           labelText: 'E-mail',
                           hintText: 'qwerty@gmail.com',
+                          controller: _emailController,
                           validator: (String? email) {
                             if (email!.isEmpty == true) {
                               return 'Please enter your email';
@@ -75,18 +80,28 @@ class _LoginPageState extends State<LoginPage> {
                         InputTextButton(
                           labelText: 'Password',
                           hintText: '*****',
+                          controller: _passwordController,
                           validator: (password) {
                             if (password!.isEmpty == true) {
                               return 'Please enter your password';
-                            } else if (password.length < 5) {
-                              return 'Password must be 5 characters or more';
+                            } else if (password.length < 6) {
+                              return 'Password must be 6 characters or more';
                             }
                             return null;
                           },
                           obscureText: true,
                         ),
                         TextButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return ForgotPasswordPage();
+                                },
+                              ),
+                            );
+                          },
                           child: Text(
                             'Forgot your password?',
                             style: TextStyle(color: black),
@@ -97,30 +112,39 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ),
               ),
-              Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 8.0,
-                      horizontal: MediaQuery.of(context).size.width / 8,
-                    ),
-                    child: BlackButton(
-                      onPressed: () {
-                        if (_formKey.currentState?.validate() ?? false) {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return const MainPage();
-                              },
-                            ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: MediaQuery.of(context).size.width / 8,
+                ),
+                child: BlackButton(
+                  onPressed: () async {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      try {
+                        await FirebaseAuth.instance.signInWithEmailAndPassword(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                        );
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'invalid-credential') {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text("Wrong password or email.")),
                           );
+                          return;
                         }
-                      },
-                      label: "Login",
-                    ),
-                  ),
-                ],
+                      }
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const MainPage();
+                          },
+                        ),
+                      );
+                    }
+                  },
+                  label: "Login",
+                ),
               ),
             ],
           ),
