7e7ebc66a9be604eb7c75c4626d456bebcf41801
rofidarashed <rofidaelmaghraby@gmail.com>
2025-04-28 20:19:23 +0300

    create payment page

---
+++ lib/pages/payment_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:uber/colors/colors.dart';
+import 'package:uber/elements/buttons/black_button.dart';
+import 'package:uber/elements/widgets/car_info.dart';
+import 'package:uber/elements/widgets/size_extensions.dart';
+import 'package:uber/pages/ride_page.dart';
+
+class PaymentPage extends StatelessWidget {
+  const PaymentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: white,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.rw, vertical: 28.rh),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CarInfo(),
+              Text('\nCharge\n', style: TextStyle(color: gray4, fontSize: 18)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Driver Fee'),
+                  Text('EGP 200', style: TextStyle(color: green)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Uber Fee'),
+                  Text('EGP 10', style: TextStyle(color: green)),
+                ],
+              ),
+              Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Total'),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '210',
+                        style: TextStyle(
+                          color: green,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 24,
+                        ),
+                      ),
+                      Transform.translate(
+                        offset: Offset(0, -9),
+                        child: Text('EGP', style: TextStyle(color: green)),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 150.rh),
+              BlackButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return RidePage();
+                      },
+                    ),
+                  );
+                },
+                label: 'pay',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
